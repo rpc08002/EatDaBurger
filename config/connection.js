4e703f2eeeaacee4d84322768fcfae96e4bd6066
rpc08002 <rcurtin0491@gmail.com>
2021-02-09 19:41:53 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
   password: 'password',
   database: 'burger_db',
 });
+};
 
 // Make connection.
 connection.connect((err) => {
@@ -19,5 +23,6 @@ connection.connect((err) => {
   console.log(`connected as id ${connection.threadId}`);
 });
 
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
