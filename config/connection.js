83943678f5bbb39faf0f255b3d2aecbea6a730ed
rpc08002 <rcurtin0491@gmail.com>
2021-02-11 16:40:23 -0500

    corrected connection error to correct database

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,23 @@
-// Set up MySQL connection.
-const mysql = require('mysql');
-var connection;
+var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'burger_db',
-});
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burger_db"
+    });
 };
 
-// Make connection.
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
-connection.connect();
-// Export connection for our ORM to use.
+// Export the connection.
 module.exports = connection;
